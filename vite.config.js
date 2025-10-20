16eb81132498f592d7b722d495a47598d4332209
lokesh-995 <pandravishamlokeshraj@gmail.com>
2025-10-20 13:09:19 +0530

    api added

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8080',
+      '/api': 'https://payflowbackend-production.up.railway.app/',
     },
   },
 });
